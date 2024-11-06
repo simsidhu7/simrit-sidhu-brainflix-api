ce6439393b91cfd7c265d18206407adc6ceb3ac6
Simrit Sidhu <simmysidhu@mac.home>
2024-11-05 19:05:13 -0500

    Push git ignore

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import fs from "fs";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
 
+const videoData = "./data/videos.json";
+
+app.listen(PORT, () =>
+  console.log(`The server is running on http://localhost:${PORT}`)
+);
+
+// app.use(cors());
+
+// app.use(express.json());
+
+// app.use("/videos.js");
---
+++ routes/videos.js
@@ -0,0 +1,9 @@
+import express from "express";
+import fs from "fs";
+import {v4 as uuidv4} from 'uuid';
+const router=express.Router();
+
+// router.get("/:id", (req,res)=>{
+//     const
+// }
+// )
